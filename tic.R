4f8c9feab5074a2af934b01f791193080f99a818
Zhian N. Kamvar <zkamvar@gmail.com>
2019-04-15 15:44:49 +0100

    add appveyor and check

--- tic.R
+++ tic.R
@@ -1,4 +1,4 @@
-# do_package_checks(warnings_are_errors = (getRversion() >= "3.2"))
+do_package_checks(warnings_are_errors = (getRversion() >= "3.2"))
 
 if (Sys.getenv("BUILD_PKGDOWN") != "") {
   get_stage("before_deploy") %>%
