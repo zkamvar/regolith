9760ba3102ac7431c5cc13b3267d514018354243
Zhian N. Kamvar <zkamvar@gmail.com>
2019-04-15 14:44:44 +0100

    modified tic to get another build

--- tic.R
+++ tic.R
@@ -2,7 +2,7 @@ do_package_checks(warnings_are_errors = (getRversion() >= "3.2"))
 
 if (Sys.getenv("BUILD_PKGDOWN") != "") {
   get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
+    add_step(step_setup_ssh()) # why is this skipping?
 
   get_stage("deploy") %>%
     add_step(step_build_pkgdown()) %>% 
